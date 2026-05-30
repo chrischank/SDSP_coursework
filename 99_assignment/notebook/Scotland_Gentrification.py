cbb186edd6584aee4964ee4cb64edb94a27275ef
Christopher Chan <chrischank@protonmail.ch>
2026-05-30 21:25:41 +0100

    homework checkpoint]

---
+++ 99_assignment/notebook/Scotland_Gentrification.py
@@ -0,0 +1,28 @@
+import marimo
+
+__generated_with = "0.23.6"
+app = marimo.App()
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Explore and Predict next Gentrification Area of Scotland based on SIMD and Housing prices
+    """)
+    return
+
+
+@app.cell
+def _():
+    import numpy as np
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
